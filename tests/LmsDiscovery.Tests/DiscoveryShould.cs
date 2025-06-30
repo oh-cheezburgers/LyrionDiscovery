0c4371f2bdb73deb22e714ecb1ed51b1a032c2a0
oh-cheezburgers <lukesuggett@gmail.com>
2025-06-30 18:08:46 +0100

    chore: re-structure folder structure.

---
+++ tests/LmsDiscovery.Tests/DiscoveryShould.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using Moq;
+using System.Net;
+using System.Text;
+
+namespace LmsDiscovery.Tests
+{
+    public class DiscoveryShould
+    {
+        private Mock<UdpClient> udpClientMock;
+        private const string handshake = "eIPAD\0NAME\0VERS\0UUID\0JSON\0CLIP\0";
+
+        public DiscoveryShould()
+        {
+            udpClientMock = new Mock<UdpClient>();
+            udpClientMock.CallBase = true;
+            
+            var from = new IPEndPoint(IPAddress.Any, 0);
+            udpClientMock.Setup(m => m.Receive(ref It.Ref<IPEndPoint>.IsAny))
+                .Callback(() => Thread.Sleep(1000))
+                .Returns(Encoding.UTF8.GetBytes(handshake));
+
+            udpClientMock.Setup(m => m.Send(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<IPEndPoint>()))
+                .Returns(1);
+        }
+
+        [Fact]
+        public void Discover()
+        {
+            //Arrange            
+            var sut = new Discoverer();
+
+            //Act
+            var response = sut.Discover(TimeSpan.FromSeconds(1), udpClientMock.Object);
+
+            //Assert
+            response.Should().Contain(handshake);
+        }
+    }
+}
